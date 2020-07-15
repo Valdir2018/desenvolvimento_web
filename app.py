16336a3f40ad96ed447292f6424e345e5a3102fb
Valdir Silva <valdirpiresba@gmail.com>
2020-07-14 23:35:14 -0300

    Criado formulario de cadastro, rotas de cadastro e conexao com banco de dados utilizando pymysql

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ from flask import Flask, redirect
 from flask.templating import render_template
 from flask.globals import request
 
+from flask.helpers import url_for
 app = Flask(__name__)
+
+conexao = pymysql.connect(host='localhost', user='root', password='', db='dbsistema_python')
+conexao_cursor = conexao.cursor()
+
+print(conexao_cursor)
 
 @app.route('/')
 def init():
@@ -12,6 +18,10 @@ def init():
 @app.route('/home/', methods=['GET'])
 def render_template_html():
     return render_template('/home/home.html')
+
+@app.route('/home/cadastrar', methods=['POST'])
+def cadastrar_dados():
+    return 'Página de Cadastrar'
 
 
 
