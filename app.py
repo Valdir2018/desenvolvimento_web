657996373e38a899d0c42eedf3a6a1d8f20c0e74
Valdir Silva <valdirpiresba@gmail.com>
2020-07-15 22:21:13 -0300

    Recebendo os dados do formulario e fazendo o primeiro insert no Mysql

--- app.py
+++ app.py
@@ -9,11 +9,10 @@ app = Flask(__name__)
 conexao = pymysql.connect(host='localhost', user='root', password='', db='dbsistema_python')
 conexao_cursor = conexao.cursor()
 
-print(conexao_cursor)
 
 @app.route('/')
 def init():
-    return 'inicio'
+    return 'Página de login'
 
 @app.route('/home/', methods=['GET'])
 def render_template_html():
@@ -21,7 +20,18 @@ def render_template_html():
 
 @app.route('/home/cadastrar', methods=['POST'])
 def cadastrar_dados():
-    return 'Página de Cadastrar'
+    if request.method == 'POST':
+        nome = request.form['nome']
+        email = request.form['email']
+        telefone = request.form['telefone']
+        senha     = request.form['senha']
+        data_criacao = datetime.datetime.now()
+
+        sql_insert = "INSERT INTO usuario(nome, email, telefone, senha,data_criacao) VALUES ('{}','{}','{}','{}','{}')".format(nome, email, telefone, senha, data_criacao)
+        conexao_cursor.execute(sql_insert)
+        conexao.commit()
+
+    return 'Cadastrado com sucesso'
 
 
 
